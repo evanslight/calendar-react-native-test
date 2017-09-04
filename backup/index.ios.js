d31568f9ed65e045bcab7140261abbc685b02ecb
evan <evanslight@live.com>
2017-09-05 00:42:35 +1000

    first try calendar

---
+++ backup/index.ios.js
@@ -0,0 +1,98 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text, TouchableOpacity, AppRegistry, View } from 'react-native';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+
+export default class AwesomeProject extends Component {
+  state = {
+    isDateTimePickerVisible: false,
+    isTimePickerVisible: false,
+    isEndTimePickerVisible: false,
+  };
+
+  _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+  _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+  _handleDatePicked = (date) => {
+    console.log('A date========== has been picked: ', date);
+    this._hideDateTimePicker();
+  };
+
+  _showTimePicker = () => this.setState({ isTimePickerVisible: true });
+
+  _hideTimePicker = () => this.setState({ isTimePickerVisible: false });
+
+  _handleTimePicked = (date) => {
+    console.log('A lalalalalalalalalal has been picked: ', date);
+    this._hideTimePicker();
+  };
+
+  _showEndTimePicker = () => this.setState({ isEndTimePickerVisible: true });
+
+  _hideEndTimePicker = () => this.setState({ isEndTimePickerVisible: false });
+
+  _handleEndTimePicked = (date) => {
+    console.log('A  endendendendendendendendendend has been picked: ', date);
+    this._hideEndTimePicker();
+  };
+
+  render () {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center',
+        alignItems: 'center', }}>
+        <View>
+        <TouchableOpacity onPress={this._showDateTimePicker}>
+          <Text>Show Datepicker</Text>
+          <Text>Show Datepicker</Text>
+        </TouchableOpacity>
+        <DateTimePicker
+          isVisible={this.state.isDateTimePickerVisible}
+          onConfirm={this._handleDatePicked}
+          onCancel={this._hideDateTimePicker}
+          mode={'date'}
+          datePickerModeAndroid='spinner'
+        />
+        </View>
+
+        <View>
+        <TouchableOpacity onPress={this._showTimePicker}>
+          <Text> </Text>
+          <Text>Show StartTimePicker</Text>
+          <Text>Show StartTimePicker</Text>
+        </TouchableOpacity>
+        <DateTimePicker
+          isVisible={this.state.isTimePickerVisible}
+          onConfirm={this._handleTimePicked}
+          onCancel={this._hideTimePicker}
+          mode={'time'}
+        />
+        </View>
+
+        <View>
+        <TouchableOpacity onPress={this._showEndTimePicker}>
+          <Text> </Text>
+          <Text>Show EndTimePicker</Text>
+          <Text>Show EndTimePicker</Text>
+        </TouchableOpacity>
+        <DateTimePicker
+          isVisible={this.state.isEndTimePickerVisible}
+          onConfirm={this._handleEndTimePicked}
+          onCancel={this._hideEndTimePicker}
+          mode={'time'}
+        />
+        </View>
+
+      </View>
+    );
+  }
+
+}
+
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
